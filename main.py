c1086a8f355b30a0f7a6f6f29880fdc0a42b488a
estie <harin0375@gmail.com>
2024-11-14 16:15:48 +0900

    feat: 기간 내 데이터 가져오기

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import os
 import time
-
+from datetime import datetime
 from dotenv import load_dotenv
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,8 +33,6 @@ def login():
     pw_element.send_keys(PASSWORD)
 
     username_element.submit()
-
-
 #search
 def load_ids():
     xlsx = pd.read_excel("peoples.xlsx",sheet_name ="main sheet")
@@ -44,21 +41,78 @@ def load_ids():
     
     return list(instagram_ids)
 
-def scraping(peoples):    
+
+def get_post_date(driver, url):
+    driver.get(url)
+    time.sleep(1)
+    elements = driver.find_element(By.XPATH, "//time[@class='x1p4m5qa']")
+    date = elements.get_attribute('title') #2024년 11월 12일
+    
+    return datetime.strptime(date, '%Y년 %m월 %d일') 
+
+def binary_search_dates(driver, posts, target):
+    left, right = 0,len(posts)-1
+    urls = list(posts.values())
+
+    while left < right:
+        mid = (left+right)//2
+        d = get_post_date(driver,urls[mid])
+        if d <= target:
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
+
+def find_post_in_date(posts,before,after): #
+    before = datetime.strptime(before, "%Y-%m-%d")
+    after = datetime.strptime(after, "%Y-%m-%d")
+    
+    start_idx = binary_search_dates(driver,posts, after)
+    #✋🏿: start_idx 다음부터 찾는걸로 리스트를 줄여도 될 듯
+    end_idx = binary_search_dates(driver,posts, before)
+
+    return (start_idx, end_idx)
+
+def sum_likes_counts(driver,sr,sc,er,ec):
+    total_likes = 0
+    total_comments = 0
+    
+    for i in range(sr, er+1):  
+        for j in range(sc if i == sr else 0, 3):  
+            if i == er and j > ec:
+                break
+            
+            focus = driver.find_element(By.XPATH, f'/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/div[2]/div/div[{i+1}]/div[{j+1}]')
+            
+            act = ActionChains(driver)
+            act.move_to_element(focus).perform()
+            time.sleep(1)
+        
+            both = focus.find_elements(By.XPATH, './a/div[3]/ul/li/span[1]/span')
+            total_likes += int(both[0].text)
+            total_comments += int(both[1].text)
+    
+    return (total_likes, total_comments)
+
+
+def scraping(peoples, before, after):    
     ids = []
     likes = []
     comments = []
+    followers = []
     
     for name in peoples:
         ids.append(name)
-        links = []
-        total_likes = 0
-        total_comments = 0
-        time.sleep(1)
-        
         url = f"https://www.instagram.com/{name}"
         driver.get(url)
+        time.sleep(1)
+        
+        #✋🏿: followers 수 추가
 
+        #####✋🏿 urls scraping - def로 변환
+        posts = {}
+        
         n = 2 #스크롤 몇번 
         for _ in range(n):
             driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
@@ -66,40 +120,46 @@ def scraping(peoples):
         
         row_posts =  driver.find_elements(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/div[2]/div/div')
 
-        for row in row_posts:
+        for r, row in enumerate(row_posts):
             threes_link = row.find_elements(By.XPATH, './div/a')
-            for p in threes_link:
-                links.append(p.get_attribute('href'))
+            for c, p in enumerate(threes_link):
+                posts[(r,c)] = p.get_attribute('href')
+        #####
+        
+        
+        ##### 기간 내 div들 가져오기.
+        start_idx, end_idx = find_post_in_date(posts,before, after)
+        keys = list(posts.keys())
+        print(start_idx, end_idx)
+        sr,sc = keys[end_idx]
+        er,ec = keys[start_idx]
+        print(sr,sc,er,ec)
+        #####
                 
-                
-            
-            threes = row.find_elements(By.XPATH, './div')
-            for p in threes:
-                act = ActionChains(driver)
-                act.move_to_element(p).perform()
-                time.sleep(1)
-                
-                both = p.find_elements(By.XPATH, './a/div[3]/ul/li/span[1]/span')
-                total_likes += int(both[0].text)
-                total_comments += int(both[1].text)
-
-
+        #####받아온 (r,c)~(r,c) 좋아요 수, 댓글 수 얻기
+        driver.get(url)
+        total_likes, total_comments = sum_likes_counts(driver,sr,sc,er,ec)
+        print(total_likes,total_comments)
         likes.append(total_likes)
         comments.append(total_comments)
+        #####
 
-        driver.quit()
 
-    return (ids,likes,comments)
+    return (ids,likes,comments,followers)
 
 
 def main():
     
     # ids = load_ids()
-    ids = ["kwak_sang_eon"]
+    ids = ["kwak_sang_eon"] #example1
     login()
     time.sleep(5)
-    results = scraping(ids)
-    ids, likes, comments = results
+    
+    before = '2024-11-14'
+    after = '2024-11-12'
+    
+    results = scraping(ids, before, after)
+    ids, likes, comments, followers = results
     
     df = pd.DataFrame(data={
     'id' : ids,
